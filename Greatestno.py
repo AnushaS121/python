dc8c419c24f8cf30b2116ab81b6125a2e340f1da
Anusha <anusha.s12107@gmail.com>
2026-06-12 08:22:55 +0530

    Greatest of three no

---
+++ Greatestno.py
@@ -0,0 +1,14 @@
+m=int(input("Enter the number for M:"))
+n=int(input("Enter the number for N:"))
+l=int(input("Enter the number for L:"))
+if(m>n):
+    if(m>l):
+        print("M is the Greatest number")
+    else:
+        print("L is the Greatest number")
+else:
+    if(n>l):
+        print("N is the Greatest number")
+    else:
+        print("L is the Greates number")
+
