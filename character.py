9bcef47ae759e2faeee5a3b4abc9ef84fbe94c5a
Anusha <anusha.s12107@gmail.com>
2026-06-12 08:24:52 +0530

    Find the Character

---
+++ character.py
@@ -0,0 +1,7 @@
+char=input("Enter any key:")
+if(char.isalpha()):
+    print("The user has enter a character")
+if(char.isdigit()):
+    print("The user has enter a digit")
+if(char.isspace()):
+    print("The user has enter a white space character")
